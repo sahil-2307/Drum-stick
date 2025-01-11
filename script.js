21e0bf426276c1e4e112c310c692b2b0d5710a43
Sahil Rajendra Bhujbal <104309864+sahil-2307@users.noreply.github.com>
2025-01-11 11:07:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 var len = document.getElementsByTagName('button').length;
-var sound = ["sounds/crash.mp3","sounds/kick-bass.mp3","sounds/snare.mp3","sounds/tom-1.mp3",
-"sounds/tom-2.mp3","sounds/tom-3.mp3","sounds/tom-4.mp3"]
+
+
 for (var i = 0;i<len;i++){
     document.getElementsByTagName('button')[i].addEventListener('click',function(){
         if (this.innerHTML==='w'){
             var audio = new Audio("sounds/crash.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='a'){
             var audio = new Audio("sounds/crash.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='a'){
             var audio = new Audio("sounds/kick-bass.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='s'){
             var audio = new Audio("sounds/snare.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='d'){
             var audio = new Audio("sounds/tom-1.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='j'){
             var audio = new Audio("sounds/tom-2.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='k'){
             var audio = new Audio("sounds/tom-3.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }else if (this.innerHTML==='l'){
             var audio = new Audio("sounds/tom-4.mp3")
-            audio.play()
+            audio.play();
+            animate(this.innerHTML);
         }
 
 
@@ -34,36 +42,52 @@ for (var i = 0;i<len;i++){
     )
 
 }
+
 document.addEventListener('keypress',function(event){
     var keyboard = event.key
     if (keyboard==='w'){
         var audio = new Audio("sounds/crash.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='a'){
         var audio = new Audio("sounds/crash.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='a'){
         var audio = new Audio("sounds/kick-bass.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='s'){
         var audio = new Audio("sounds/snare.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='d'){
         var audio = new Audio("sounds/tom-1.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='j'){
         var audio = new Audio("sounds/tom-2.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='k'){
         var audio = new Audio("sounds/tom-3.mp3")
         audio.play()
+        animate(keyboard);
     }else if (keyboard==='l'){
         var audio = new Audio("sounds/tom-4.mp3")
         audio.play()
+        animate(keyboard);
     }
 })
 
+function animate(keyboard){
+    var a = document.querySelector('.'+ keyboard);
+    a.classList.add('pressed');
 
+    setTimeout(function(){
+        a.classList.remove('pressed');
+    },100);
+}
 
 
 
